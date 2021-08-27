80abf51620bd65c67e0d2b81f119aa5e5f42321e
HanGilyong <shkg13@naver.com>
2021-08-27 21:26:33 +0900

    최초프로젝트생성

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num =0;
+		num = sc.nextInt();
+		System.out.println("1. 한길용 2. 이진원 3. 진동수 4. 이창훈 5. 이동호");
+		switch(num) {
+		case 1 : break;
+		case 2 : break;
+		case 3 : break;
+		case 4 : break;
+		case 5 : break;
+		}
+	}
+}
